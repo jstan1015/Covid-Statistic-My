9f8a60b4c91502a1bbed76bb6b28b3dd42d6ddc1
jstan1015 <jstan991015@gmail.com>
2023-03-28 15:15:48 +0800

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/Covid-Statistic-My/'
-    : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/Covid-Statistic-My/' : '/'
 }
